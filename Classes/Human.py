5c05d75c37bbfca946abbeefbcbc7fb88d94cf9e
Precious Yankson <maamearaba1612@gmail.com>
2023-10-03 05:45:31 -0700

    An updated code

--- Classes/Human.py
+++ Classes/Human.py
@@ -19,9 +19,21 @@ class Human:
         self.number_of_ears = number_of_ears
         self.mouth = mouth
 
+    def clap(self):
+        available_hands = self.number_of_hands
+        return f"clapping with {available_hands} hands"
 
+    
 human1 = Human("Stanley", 2, 2, 2, 2, 1)
 print(human1.name)
 print(type(human1))
 
+
+stanley =Human("stanley", 2, 2, 1, 2, 1)
+dice = Human("dice", 2, 2, 2, 1, 2)
+print(stanley.name)
+print(f"{stanley.name} is {stanley.clap()}")
+print(f"{dice.name} is {dice.clap()}")
+print(type(stanley))
+print(stanley.clap())
 
